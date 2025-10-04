6112f23575a28179d5c80212dbafbacf33322550
Manas Chaudhary <manaschaudhary1602@gmail.com>
2025-10-04 22:08:31 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
 //your JS code here. If required.
+function updateClock() {
+  const now = new Date(); // current date & time
+  const p = document.querySelector("#timer");
+  p.textContent =now.toLocaleDateString() +", " + now.toLocaleTimeString("en-US", { hour12: true }); // display system time
+}
+
+updateClock(); // run once at start
+setInterval(updateClock, 1000);
